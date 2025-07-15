a66522057f50ab61bad0effbe463d593c788eaca
adiii6767 <90821041+adiii6767@users.noreply.github.com>
2025-07-15 20:18:39 +0530

    Add files via upload

---
+++ src/app/app.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Navbar } from './navbar/navbar';
+import { Footer } from './footer/footer';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, Navbar, Footer],
+  templateUrl: './app.html',
+  styleUrls: ['./app.css']
+})
+export class App {}
